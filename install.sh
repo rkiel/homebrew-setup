61b2886e6c17125b4d2b6f07759fe1caf82bc65c
Robert Kiel <cygnus@pobox.com>
2020-11-22 10:58:08 -0500

    Update install.sh
    rvm

--- install.sh
+++ install.sh
@@ -80,6 +80,11 @@ if [ ! -e $TOUCH ] ; then
   rvm install $RUBY_CURRENT
   rvm install $RUBY_PREVIOUS
   rvm --default use $RUBY_CURRENT
+  # bashrc
+  # # Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+# export PATH="$PATH:$HOME/.rvm/bin"
+# profile
+# [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
   touch $TOUCH
 fi
 
